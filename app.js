1fc827915e89bc6a695d98b6c1e785237363f9f9
elvis <georgej29942@gmail.com>
2026-03-17 17:54:04 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const { engine } = require('express-handlebars');
-const fs = require('fs');
-const path = require('path');
+import express from 'express';
+import { engine } from 'express-handlebars';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
